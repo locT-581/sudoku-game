c08d190c2fe864f91a736750f2ad44141438606b
tnhl <loct.se581@gmail.com>
2023-09-08 21:48:41 +0700

    Basic Setting

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,25 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Lottie from 'react-lottie';
 import icon from '../../assets/icon.svg';
+import animationData from './Test.json';
 import './App.css';
 
 function Hello() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <div>
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>Sudoku Game</h1>
+      <Lottie options={defaultOptions} height={400} width={400} />
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
